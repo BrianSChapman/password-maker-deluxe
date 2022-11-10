da43d4e631b61e985e9d0ced133e87ec64c68c80
Brian Chapman <chapman.brian86@gmail.com>
2022-11-09 21:41:57 -0500

    Reworking conditional statements for password length

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var Upper = 0;
+var Lower = 0;
+var Special = 0;
+var Numeric = 0;
+
 
 function generatePassword() {
 
+  var userLength = window.prompt("Please choose a password length, new friend!")  
+
+if (userLength < 8 || userLength > 128) {
+  window.alert("Sorry friend! Password must be between 8 and 128 characters.")
+  return
+ } 
 }
-var userLength = window.prompt("Please choose a password length, new friend!")  
+  if (confirm("would you like to use lower case letters in it?")) { 
+    Lower++;  
+  }  
+ 
+
+  // window.confirm("You cool with uppercase letters?");
+  // window.confirm("How about random numbers? You a random numbers type?");
+  // window.confirm("You want special, fancy characters?");
+
+
 
 var characterLength = function getrandomInt(min, max) {
   min = Math.ceil(8);
   max = Math.floor(128);
   return Math.floor(Math.random()) * ((max - min) + min);
-}  
-if (!characterLength) {
-  window.alert("Sorry friend! Password must be between 8 and 128 characters.");
-} else if (
-  window.confirm("Would you like to use lowercase letters in it?")) 
-
+}
 
 // Write password to the #password input
 function writePassword() {
