e290c852f30b8826ab1272e96dea0ab36aec0baf
Brian Chapman <chapman.brian86@gmail.com>
2022-11-09 20:17:45 -0500

    Worked out initial variables and window prompt for password length

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var buttonclick = window.prompt("Please choose the length of your password");
+
+function generatePassword() {
+
+}
+var userLength = window.prompt("Please choose a password length, new friend!")  
+
+var characterLength = function getrandomInt(min, max) {
+  min = Math.ceil(8);
+  max = Math.floor(128);
+  return Math.floor(Math.random()) * ((max - min) + min);
+}  
+if (!characterLength) {
+  window.alert("Sorry friend! Password must be between 8 and 128 characters.");
+} else if (
+  window.confirm("Would you like to use lowercase letters in it?")) 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,4 +28,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-var buttonclick = window.prompt("Please choose the length of your password");
